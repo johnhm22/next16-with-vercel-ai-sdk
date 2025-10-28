ad76d4ce15723e27c0b2686f6268152958c68e12
johnhm22 <johnhowardmorgan@outlook.com>
2025-10-28 15:21:24 +0000

    Changing model to anthropic claude-sonnet-4

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -1,11 +1,13 @@
 import { streamText } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { anthropic } from "@ai-sdk/anthropic";
 
 export async function POST(req: Request) {
 	try {
 		const { prompt } = await req.json();
 		const result = streamText({
-			model: openai("gpt-4.1-nano"),
+			// model: openai("gpt-4.1-nano"),
+			model: anthropic("claude-sonnet-4-20250514"),
 			prompt,
 		});
 		return result.toUIMessageStreamResponse();
