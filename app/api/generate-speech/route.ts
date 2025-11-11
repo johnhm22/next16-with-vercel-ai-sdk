66cdb74782f68bdeed7f7e6576d5f5d5380c48ce
johnhm22 <johnhowardmorgan@outlook.com>
2025-11-11 12:21:48 +0000

    Creating api ai route for text to speech feature

---
+++ app/api/generate-speech/route.ts
@@ -0,0 +1,22 @@
+import { openai } from "@ai-sdk/openai";
+import { experimental_generateSpeech as generateSpeech } from "ai";
+
+export async function POST(req: Request) {
+	try {
+		const { text } = await req.json();
+
+		const { audio } = await generateSpeech({
+			model: openai.speech("tts-1"),
+			text,
+		});
+
+		return new Response(audio.uint8Array as BodyInit, {
+			headers: {
+				ContentType: audio.mediaType || "audio/mpeg",
+			},
+		});
+	} catch (error) {
+		console.error("Couldn't create an audio file", error);
+		return new Response("Failed to generate speech", { status: 500 });
+	}
+}
