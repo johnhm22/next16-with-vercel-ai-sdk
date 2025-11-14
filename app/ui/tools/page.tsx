c7374a155eb8c21d767bb672c8ad5844bd562c8e
johnhm22 <johnhowardmorgan@outlook.com>
2025-11-14 11:12:55 +0000

    Creating page calling api with tool object containing getWeather function

---
+++ app/ui/tools/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React, { useState } from "react";
+import { useChat } from "@ai-sdk/react";
+import { DefaultChatTransport } from "ai";
+
+export default function ToolsChatPage() {
+	const [input, setInput] = useState("");
+
+	const { messages, sendMessage, status, error, stop } = useChat({
+		transport: new DefaultChatTransport({
+			api: "/api/tools",
+		}),
+	});
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		sendMessage({ text: input });
+		setInput("");
+	};
+
+	return (
+		<div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+			{error && <div className="text-red-500 mb-4">{error.message}</div>}
+			{messages.map((message) => (
+				<div className="mb-4" key={message.id}>
+					<div className="font-semibold">
+						{message.role === "user" ? "You:" : "AI:"}
+					</div>
+					{message.parts.map((part, idx) => {
+						switch (part.type) {
+							case "text":
+								return (
+									<div
+										className="whitespace-pre-wrap"
+										key={`${message.id}-${idx}`}
+									>
+										{part.text}
+									</div>
+								);
+							default:
+								return null;
+						}
+					})}
+				</div>
+			))}
+
+			{(status === "submitted" || status === "streaming") && (
+				<div className="mb-4">
+					<div className="flex items-center gap-2">
+						<div className="animate-spin rounded-full h-4 w-4 border-b-2 border-blue-400"></div>
+					</div>
+				</div>
+			)}
+
+			<form
+				onSubmit={handleSubmit}
+				className="mb-3 fixed bottom-0 mx-auto w-full bg-zinc-50 left-0 right-0 max-w-md"
+			>
+				<div className="flex gap-2">
+					<input
+						className="p-2 border border-zinc-300 flex-1 rounded-lg shadow-xl"
+						placeholder="How can I help you?"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						suppressHydrationWarning
+					/>
+
+					{status === "streaming" || status === "submitted" ? (
+						<button
+							onClick={stop}
+							className=" disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500 text-white py-2 px-4 hover:bg-blue-700 rounded"
+							type="submit"
+						>
+							Stop
+						</button>
+					) : (
+						<button
+							className=" disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500 text-white py-2 px-4 hover:bg-blue-700 rounded"
+							type="submit"
+							disabled={status !== "ready"}
+						>
+							Send
+						</button>
+					)}
+				</div>
+			</form>
+		</div>
+	);
+}
