7f23ae3a3cf9cd72567150ef5e6532bd2cbccb0e
johnhm22 <johnhowardmorgan@outlook.com>
2025-11-17 10:31:12 +0000

    Adding code to show ai responses based on part type and state

--- app/ui/tools/page.tsx
+++ app/ui/tools/page.tsx
@@ -3,15 +3,19 @@
 import React, { useState } from "react";
 import { useChat } from "@ai-sdk/react";
 import { DefaultChatTransport } from "ai";
+import { ChatMessage } from "@/app/api/tools/route";
 
 export default function ToolsChatPage() {
 	const [input, setInput] = useState("");
 
-	const { messages, sendMessage, status, error, stop } = useChat({
+	const { messages, sendMessage, status, error, stop } = useChat<ChatMessage>({
 		transport: new DefaultChatTransport({
 			api: "/api/tools",
 		}),
 	});
+
+	//check details of messages returned from api
+	// messages.map((message) => console.log("MESSAGE: ", message));
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -38,8 +42,52 @@ export default function ToolsChatPage() {
 										{part.text}
 									</div>
 								);
-							default:
-								return null;
+							case "tool-getWeather":
+								switch (part.state) {
+									case "input-streaming":
+										return (
+											<div key={`${message.id}-getWeather-${idx}`}>
+												<div className="text-sm text-zinc-500">
+													🌤️ Receiving weather request...
+												</div>
+												<pre className="text-xs text-zinc-600 mt-1">
+													{JSON.stringify(part.input, null, 2)}
+												</pre>
+											</div>
+										);
+									//This case is not available in the model being used
+									// case "input-available":
+									// 	return;
+									// 	<div key={`${message.id}-getWeather-${idx}`}>
+									// 		<div className="bg-zinc-800/50 border border-zinc-700 rounded text-zinc-500">
+									// 			Getting weather for {part.input.city}...
+									// 		</div>
+									// 	</div>;
+									case "output-available":
+										return (
+											<div key={`${message.id}-getWeather-${idx}`}>
+												<div className="bg-zinc-400/50 border border-zinc-700 rounded my-3 p-1">
+													<div className="text-sm text-zinc-400">Weather</div>
+													<div className="text-sm text-zinc-700">
+														<div>{part.input.city}</div>
+														<div>🌤️ {part.output}</div>
+													</div>
+												</div>
+											</div>
+										);
+									case "output-error":
+										return (
+											<div key={`${message.id}-getWeather-${idx}`}>
+												<div className="bg-zinc-400/50 border border-zinc-700 rounded text-zinc-500 my-3 p-1">
+													<div className="text-sm text-red-400">
+														Error: {part.errorText}
+													</div>
+												</div>
+											</div>
+										);
+									default:
+										return null;
+								}
 						}
 					})}
 				</div>
