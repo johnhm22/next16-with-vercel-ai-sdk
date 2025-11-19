a945940cefa196b6ee6e5a7c8538ad395d1b8c54
johnhm22 <johnhowardmorgan@outlook.com>
2025-11-19 11:25:59 +0000

    Adding in call to weatherapi

---
+++ app/api/api-tool/route.ts
@@ -0,0 +1,67 @@
+import { openai } from "@ai-sdk/openai";
+import {
+	UIMessage,
+	UIDataTypes,
+	InferUITools,
+	streamText,
+	convertToModelMessages,
+	tool,
+	stepCountIs,
+} from "ai";
+import { z } from "zod";
+
+const tools = {
+	getWeather: tool({
+		description: "Get the weather for a defined location",
+		inputSchema: z.object({
+			city: z.string().describe("The city for which the weather is required"),
+		}),
+		execute: async ({ city }) => {
+			const response = await fetch(
+				`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
+			);
+			const data = await response.json();
+
+			console.log("data in api: ", data);
+
+			const weatherData = {
+				location: {
+					name: data.location.name,
+					country: data.location.country,
+					localtime: data.location.localtime,
+				},
+				current: {
+					temp_c: data.current.temp_c,
+					condition: {
+						text: data.current.text,
+						code: data.current.condition.code,
+					},
+				},
+			};
+
+			console.log("weatherData: ", weatherData);
+			return weatherData;
+		},
+	}),
+};
+
+export type ChatTools = InferUITools<typeof tools>;
+export type ChatMessage = UIMessage<never, UIDataTypes, ChatTools>;
+
+export async function POST(req: Request) {
+	try {
+		const { messages }: { messages: ChatMessage[] } = await req.json();
+		// console.log("messages in api: ", messages[0].parts);
+		const result = streamText({
+			model: openai("gpt-5-mini"),
+			messages: convertToModelMessages(messages),
+			tools,
+			stopWhen: stepCountIs(3),
+		});
+
+		return result.toUIMessageStreamResponse();
+	} catch (error) {
+		console.error("Error streaming chat", error);
+		return new Response("Failed to stream chat completion", { status: 500 });
+	}
+}
