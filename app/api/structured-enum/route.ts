9b930574e86b53a09d022653216db1201ac341f7
johnhm22 <johnhowardmorgan@outlook.com>
2026-01-26 15:40:31 +0000

    Trying to update from deprecated function

--- app/api/structured-enum/route.ts
+++ app/api/structured-enum/route.ts
@@ -1,17 +1,26 @@
-import { generateObject } from "ai";
+import { generateObject, generateText, Output } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { z } from "zod";
 
 export async function POST(req: Request) {
 	const { text } = await req.json();
 	try {
+		//generateObject is now deprecated. generateText recommended, but getting errors.
 		const result = await generateObject({
 			model: openai("gpt-4.1-mini"),
 			output: "enum",
 			enum: ["positive", "negative", "neutral"],
 			prompt: `Classify a sentiment in this text: ${text}`,
 		});
+		// const { output } = await generateText({
+		// 	model: openai("gpt-4.1-mini"),
+		// 	output: Output.array({
+		// 		element: z.enum(["positive", "negative", "neutral"]),
+		// 	}),
+		// 	prompt: `Classify a sentiment in this text: ${text}`,
+		// });
 
-		console.log("response from AI call: ", result.toJsonResponse());
+		// console.log("response from AI call: ", result.toJsonResponse());
 
 		return result.toJsonResponse();
 	} catch (error) {
